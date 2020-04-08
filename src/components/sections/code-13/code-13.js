11ea8d9808fdd71ac893c8c7b32263c4ed7960b2
Qian Chen <elgs1980@hotmail.com>
2020-04-07 20:48:36 -0700

    Update.

---
+++ src/components/sections/code-13/code-13.js
@@ -0,0 +1,28 @@
+import LWElement from './../../../lib/lw-element.js';
+import interpolation from './ast.js';
+
+const component = { id: 'leanweb-app-sections-code-13', interpolation };
+customElements.define(component.id,
+   class extends LWElement {  // LWElement extends HTMLElement
+      constructor() {
+         super(component);
+         this.shadowRoot.querySelectorAll('pre code').forEach((block) => {
+            hljs.highlightBlock(block);
+         });
+      }
+      array2d = [
+         [1, 2, 3],
+         [4, 5, 6],
+         [7, 8, 9],
+      ];
+
+      reset() {
+         this.array2d = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+         ];
+         this.update();
+      }
+   }
+);
