76727c2649bffe76322b48944a1217be47395da6
Qian Chen <elgs1980@hotmail.com>
2020-05-03 19:59:20 -0700

    Update.

--- src/components/section-primary/section-primary.js
+++ src/components/section-primary/section-primary.js
@@ -5,13 +5,13 @@ import hljs from 'highlight.js/lib/core.js';
 import javascript from 'highlight.js/lib/languages/javascript.js';
 import xml from 'highlight.js/lib/languages/xml.js';
 import scss from 'highlight.js/lib/languages/scss.js';
-
-// import 'highlight.js/styles/agate.css';
+import agate from 'highlight.js/scss/agate.scss';
 
 customElements.define('leanweb-app-section-primary',
    class extends LWElement {  // LWElement extends HTMLElement
       constructor() {
          super(ast);
+
          this.terminalData = [
             'npm install leanweb -g',
             'mkdir demo && cd demo',
@@ -19,6 +19,7 @@ customElements.define('leanweb-app-section-primary',
             'lw serve',
          ];
 
+         super.applyStyles(agate);
          hljs.registerLanguage('javascript', javascript, xml);
          hljs.registerLanguage('html', xml);
          hljs.registerLanguage('scss', scss);
--- src/components/sections/code-11/code-11.js
+++ src/components/sections/code-11/code-11.js
@@ -5,11 +5,13 @@ import hljs from 'highlight.js/lib/core';
 import javascript from 'highlight.js/lib/languages/javascript';
 import xml from 'highlight.js/lib/languages/xml';
 import scss from 'highlight.js/lib/languages/scss';
+import agate from 'highlight.js/scss/agate.scss';
 
 customElements.define('leanweb-app-sections-code-11',
    class extends LWElement {  // LWElement extends HTMLElement
       constructor() {
          super(ast);
+         super.applyStyles(agate);
          hljs.registerLanguage('javascript', javascript, xml);
          hljs.registerLanguage('html', xml);
          hljs.registerLanguage('scss', scss);
