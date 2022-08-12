b598c2cf72af0abb6ea5543a30fe2e169d088638
Qian Chen <elgs1980@hotmail.com>
2022-08-12 03:40:05 -0700

    Update.

---
+++ src/components/sections/code-00/code-00.js
@@ -0,0 +1,23 @@
+import LWElement from './../../../lib/lw-element.js';
+import ast from './ast.js';
+
+import hljs from '/node_modules/highlight.js/lib/core.js';
+import javascript from '/node_modules/highlight.js/lib/languages/javascript.js';
+import xml from '/node_modules/highlight.js/lib/languages/xml.js';
+import scss from '/node_modules/highlight.js/lib/languages/scss.js';
+
+customElements.define('leanweb-app-sections-code-00',
+   class extends LWElement {  // LWElement extends HTMLElement
+      constructor() {
+         super(ast);
+         hljs.registerLanguage('javascript', javascript);
+         hljs.registerLanguage('html', xml);
+         hljs.registerLanguage('scss', scss);
+         this.shadowRoot.querySelectorAll('pre code').forEach((block) => {
+            hljs.highlightElement(block);
+         });
+      }
+
+      world = 'Leanweb';
+   }
+);
