887493542d18ee09469a62d1d93abcd69cdc24e8
Qian Chen <elgs1980@hotmail.com>
2024-03-31 18:36:56 -0700

    Update.

--- src/components/sections/code-12/code-12.js
+++ src/components/sections/code-12/code-12.js
@@ -4,7 +4,7 @@ import ast from './ast.js';
 import hljs from 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/es/core.min.js';
 import javascript from 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/es/languages/javascript.min.js';
 import xml from 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/es/languages/xml.min.js';
-import scss from 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/es/languages/scss.min.js';
+import css from 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/es/languages/css.min.js';
 
 
 customElements.define('leanweb-app-sections-code-12',
@@ -14,7 +14,7 @@ customElements.define('leanweb-app-sections-code-12',
          
          hljs.registerLanguage('javascript', javascript);
          hljs.registerLanguage('html', xml);
-         hljs.registerLanguage('scss', scss);
+         hljs.registerLanguage('css', css);
          this.shadowRoot.querySelectorAll('pre code').forEach((block) => {
             hljs.highlightElement(block);
          });
